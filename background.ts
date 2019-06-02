c9c6cec7e68bffc9eccfb4b5c0cf1a71b850be9d
Bassam Helal <basshelal@gmail.com>
2019-06-02 09:16:47 +0100

    Fixed initialization bug and added new icons

---
+++ background.ts
@@ -0,0 +1,22 @@
+///<reference path="../../.WebStorm2019.1/config/javascript/extLibs/global-types/node_modules/@types/chrome/index.d.ts"/>
+
+import InstalledDetails = chrome.runtime.InstalledDetails;
+
+/**
+ * Runs on install to check if the storage has been initialized or not, and if not will initialize with default values
+ */
+chrome.runtime.onInstalled.addListener((details: InstalledDetails) => {
+
+    chrome.storage.sync.get(["textSize", "lineHeight", "onOff", "font", "whitelisted"], (fromStorage) => {
+
+        if (!fromStorage.textSize || !fromStorage.lineHeight || !fromStorage.onOff || !fromStorage.font || !fromStorage.whitelisted) {
+            chrome.storage.sync.set({
+                textSize: '115',
+                lineHeight: '125',
+                onOff: true,
+                font: "Droid Arabic Naskh",
+                whitelisted: []
+            });
+        }
+    });
+});
