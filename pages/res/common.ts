30613b588785ad5106394e3f325330dc21a4abed
Bassam Helal <basshelal@gmail.com>
2020-02-17 10:09:41 +0000

    Cleaned up common pages css functionality into single common.css file, js common to come later

---
+++ pages/res/common.ts
@@ -0,0 +1,3 @@
+// Add these 2:
+// <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+// <script defer src="https://code.getmdl.io/1.3.0/material.min.js"></script>
