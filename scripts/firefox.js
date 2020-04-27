1224f585cf92fc832c6798c8490cdc37dbe49709
Bassam Helal <basshelal@gmail.com>
2020-04-27 02:17:45 +0100

    Saving progress

--- scripts/firefox.js
+++ scripts/firefox.js
@@ -1,1 +1,7 @@
 // Find all instances of "chrome-extension" across all files and replace them with "moz-extension"
+var replace = require('replace-in-file');
+var options = {
+    files: "../src/*",
+    from: /chrome-extension/g,
+    to: "moz-extension",
+};
--- scripts/firefox.ts
+++ scripts/firefox.ts
@@ -1,1 +1,8 @@
 // Find all instances of "chrome-extension" across all files and replace them with "moz-extension"
+
+const replace = require('replace-in-file');
+const options = {
+    files: "../src/*",
+    from: /chrome-extension/g,
+    to: "moz-extension",
+};
